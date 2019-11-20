3ceb8fad30ee192ea7d6e5ab23ddbd0f5a3d791b
OraculoTrainingen <dev@oraculo.nl>
2019-11-20 22:40:55 +0100

    add 2 more mappings to ArtiestEndpoint

--- src/main/java/nl/oraculo/popsongs/endpoint/ArtiestEndpoint.java
+++ src/main/java/nl/oraculo/popsongs/endpoint/ArtiestEndpoint.java
@@ -3,9 +3,9 @@ package nl.oraculo.popsongs.endpoint;
 import nl.oraculo.popsongs.domain.Artiest;
 import nl.oraculo.popsongs.service.ArtiestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "artiest")
@@ -15,5 +15,13 @@ public class ArtiestEndpoint {
     @GetMapping
     public Iterable<Artiest> findAll() {
         return artiestService.findAll();
+    }
+    @GetMapping(path = "{id}")
+    public Optional<Artiest> findById(@PathVariable int id) {
+        return artiestService.findById(id);
+    }
+    @PostMapping
+    public Artiest save(@RequestBody Artiest artiest) {
+        return artiestService.save(artiest);
     }
 }

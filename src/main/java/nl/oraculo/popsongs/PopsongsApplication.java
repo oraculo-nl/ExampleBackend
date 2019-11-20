2ae29afc0fb4d519d6875530893dcdadf79753d4
OraculoTrainingen <dev@oraculo.nl>
2019-11-20 13:46:40 +0100

    first commit

---
+++ src/main/java/nl/oraculo/popsongs/PopsongsApplication.java
@@ -0,0 +1,13 @@
+package nl.oraculo.popsongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopsongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopsongsApplication.class, args);
+	}
+
+}

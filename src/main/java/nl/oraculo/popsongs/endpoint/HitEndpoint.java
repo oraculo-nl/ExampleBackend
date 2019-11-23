09986c06dea4756edd6a2825464e384e1ab50256
OraculoTrainingen <dev@oraculo.nl>
2019-11-23 22:19:03 +0100

    add crossorigin entries

--- src/main/java/nl/oraculo/popsongs/endpoint/HitEndpoint.java
+++ src/main/java/nl/oraculo/popsongs/endpoint/HitEndpoint.java
@@ -3,10 +3,12 @@ package nl.oraculo.popsongs.endpoint;
 import nl.oraculo.popsongs.domain.Hit;
 import nl.oraculo.popsongs.service.HitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(path = "hit")
 public class HitEndpoint {
